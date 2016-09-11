127569bb270ef8625751ab2c31a7c85362f52201
owen-d <ow.diehl@gmail.com>
2016-09-11 19:28:34 -0400

    recursive refactor

--- src/erastosthenes/core.clj
+++ src/erastosthenes/core.clj
@@ -1,59 +1,30 @@
 (ns erastosthenes.core)
 
-(defn modulimit
+(defn moduLimit
 	"Builds a gating modulo fn"
 	[x]
-	#(mod % x))
+	(fn
+		([a] (if (or (= a nil) (= (mod a x) 0))
+			; is a multiple or was a multiple of a previous gate, disregard.
+			nil
+			;
+			a
+			))
+		; if passed no argument, we're on the final step. Return the original val this gate was made from,
+		 ; as it's a valid prime.
+		([] x)))
 
-; (defn doEet
-; 	[arr reducer]
-; 	(doseq arr reducer))
+(defn recursivePrimes
+	[upperbound pipe value]
+	(cond
+		; we're done: return all of the gates
+		(> value upperbound) (map #(%) pipe)
+		;
+		(boolean (reduce #(%2 %1) value pipe)) (recursivePrimes upperbound (conj pipe (moduLimit value)) (+ value 1))
+		:else (recursivePrimes upperbound pipe (+ value 1))))
 
-(defn gauntlet
-	[accum, gate]
-	(if accum (gate accum) accum))
-
-
-(defn idkyet
-	"still figuring this shit out"
-	[x]
-	(let [inputs (range 2 (+ x 1))
-		gates []
-		res []]
-		(doseq [value inputs]
-			(if 
-				(reduce 
-					gauntlet
-					value
-					gates)
-				(do (conj gates (modulimit value))
-					(conj res value)
-					(println (str "hit" value gates)))))))
-; 
-; 
-; 
-; build vector/generator
-; reference a constructed array
-; reduce through said array. if reaches end, push to results & push a new limiter fn to results arr
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
 (defn -main
   "I don't do a whole lot."
   []
-  (idkyet 5))
-
-; (defn hof [a] (fn [b] (= a b)))
-; (defn hof [a] #(= a %1))
+  (let [gate (moduLimit 2)]
+  	(println (reduce #(str %1 ", " %2) (recursivePrimes 500 [] 2)))))

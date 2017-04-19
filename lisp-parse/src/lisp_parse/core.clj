8c1ba1752fd30cc8545c78c3f2e6a779ef846b4d
Owen Diehl <ow.diehl@gmail.com>
2017-04-19 17:28:18 -0400

    naive solution, doesnt handle word splitting yet, only chars

--- lisp-parse/src/lisp_parse/core.clj
+++ lisp-parse/src/lisp_parse/core.clj
@@ -9,7 +9,8 @@
 (defn parse
   "parses list strings"
   [x]
-  ())
+  (first (next_expr
+          (clean_input x))))
 
 (defn clean_input
   [input]
@@ -18,21 +19,26 @@
 
 (defn next_expr
   "fetch next list, returning [next-expr rest-of-unparsed-string]"
-  ([preparsed] (next_expr preparsed [] ["(" ")"]))
+  ([preparsed] (next_expr preparsed ["(" ")"]))
+  ([preparsed [start_symbol end_symbol]]
+   (let [next_symbol (str (first preparsed))
+         rest (drop 1 preparsed)]
+     (if (= next_symbol start_symbol)
+       (next_expr rest [start_symbol end_symbol])
+       (next_expr rest [next_symbol] [start_symbol end_symbol]))))
   ([preparsed cur_col [start_symbol end_symbol]]
    (let [next_symbol (str (first preparsed))
          rest (drop 1 preparsed)]
      (cond
        ;; we have a new list
-           (= next_symbol start_symbol) (next_expr rest cur_col [start_symbol end_symbol])
-                                        ;end of the list has been reached!
-           (= next_symbol end_symbol) [cur_col rest]
-                                        ;we're in the middle of a list
+       (= next_symbol start_symbol) (let [[nestled_expr new_rest] (next_expr rest)]
+                                      (next_expr new_rest (conj cur_col nestled_expr) [start_symbol end_symbol]))
+       ;end of the list has been reached!
+       (= next_symbol end_symbol) [cur_col rest]
+       ;we're in the middle of a list
        :else (next_expr rest (conj cur_col next_symbol) [start_symbol end_symbol])))))
 
-(first (next_expr (clean_input "(* (+ 1 2) 3)")))
+;; (first (next_expr (clean_input "(* (+ 1 2) 3)")))
 
-
-;; (clean_input "(* (+ 1 2) 3)")
-
+(parse "(* 2 (+ 1 2))")
 
